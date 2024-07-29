d6cf8759f33069b039600d85ad9ff16907d471f3
simd-float4x4 <asobu.developers+Mathew@gmail.com>
2024-07-29 20:12:17 +0900

    fix

--- config/initializers/cors.rb
+++ config/initializers/cors.rb
@@ -1,6 +1,6 @@
 Rails.application.config.middleware.insert_before 0, Rack::Cors do
     allow do
-        origins ENV['FRONTEND_ORIGIN_URL'], ENV['FRONTEND_DEVELOPMENT_URL'],
+        origins ENV['FRONTEND_ORIGIN_URL'], ENV['FRONTEND_DEVELOPMENT_URL']
         resource '*',
           headers: :any,
           methods: [:get, :post, :patch, :put, :delete, :options, :head],
